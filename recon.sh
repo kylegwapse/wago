98fc95b5be2b621e8c1d13a33f262d55fdcce2bd
kylegwapse <30412970+kylegwapse@users.noreply.github.com>
2020-11-23 19:49:59 +0800

    Update recon.sh

--- recon.sh
+++ recon.sh
@@ -1,5 +1,4 @@
-echo "* * * * * root /bin/bash /etc/ohpserver/stop 2>/dev/null" > /etc/cron.d/recon
-echo "* * * * * root /bin/bash /etc/ohpserver/run 2>/dev/null" > /etc/cron.d/recon
+echo "* * * * * root /bin/bash /etc/ohpserver/stop 2>/dev/null" > /etc/cron.d/rc
 service cron restart
 echo 'Server-side Auto Recon Complete'
 echo 'Setup Done by Kashi Estudillo'
