b95178d0403b4c2df20475cd5ebfa59398a28c55
kylegwapse <30412970+kylegwapse@users.noreply.github.com>
2020-12-02 11:37:48 +0800

    Update l2tp-prem.php

--- l2tp-prem.php
+++ l2tp-prem.php
@@ -1,34 +1,111 @@
-
 <?php
 error_reporting(E_ERROR | E_PARSE);
 ini_set('display_errors', '1');
-//include('config.php');
 
-
-$DB_host = '64.20.63.202';
-$DB_user = 'ehisshco_regina';
-$DB_pass = 'Regina143';
-$DB_name = 'ehisshco_regina';
-
+$DB_host = "64.20.63.202";
+$DB_user = "ehisshco_flyguard";
+$DB_pass = "flyguard123";
+$DB_name = "ehisshco_flyguard";
 
 $mysqli = new MySQLi($DB_host,$DB_user,$DB_pass,$DB_name);
 if ($mysqli->connect_error) {
     die('Error : ('. $mysqli->connect_errno .') '. $mysqli->connect_error);
 }
 
+function encrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $encryptedPassword=encryptPaswd($paswd,$mykey);
+	  return $encryptedPassword;
+	}
+	 
+	// function to get the decrypted user password
+	function decrypt_key($paswd)
+	{
+	  $mykey=getEncryptKey();
+	  $decryptedPassword=decryptPaswd($paswd,$mykey);
+	  return $decryptedPassword;
+	}
+	 
+	function getEncryptKey()
+	{
+		$secret_key = md5('eugcar');
+		$secret_iv = md5('sanchez');
+		$keys = $secret_key . $secret_iv;
+		return encryptor('encrypt', $keys);
+	}
+	function encryptPaswd($string, $key)
+	{
+	  $result = '';
+	  for($i=0; $i<strlen ($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)+ord($keychar));
+		$result.=$char;
+	  }
+		return base64_encode($result);
+	}
+	 
+	function decryptPaswd($string, $key)
+	{
+	  $result = '';
+	  $string = base64_decode($string);
+	  for($i=0; $i<strlen($string); $i++)
+	  {
+		$char = substr($string, $i, 1);
+		$keychar = substr($key, ($i % strlen($key))-1, 1);
+		$char = chr(ord($char)-ord($keychar));
+		$result.=$char;
+	  }
+	 
+		return $result;
+	}
+	
+	function encryptor($action, $string) {
+		$output = false;
+
+		$encrypt_method = "AES-256-CBC";
+		//pls set your unique hashing key
+		$secret_key = md5('eugcar sanchez');
+		$secret_iv = md5('sanchez eugcar');
+
+		// hash
+		$key = hash('sha256', $secret_key);
+		
+		// iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+		$iv = substr(hash('sha256', $secret_iv), 0, 16);
+
+		//do the encyption given text/string/number
+		if( $action == 'encrypt' ) {
+			$output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+			$output = base64_encode($output);
+		}
+		else if( $action == 'decrypt' ){
+			//decrypt the given text/string/number
+			$output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+		}
+
+		return $output;
+	}
+
+
+
 $data = '';
-#Active Accounts
-$premium_active = "is_validated=1 AND frozen=0 AND duration > 0";
-$vip_active = "is_validated=1 AND frozen=0 AND vip_duration > 0";
-$private_active = "is_validated=1 AND frozen=0 AND private_duration > 0";
-$query = $mysqli->query("SELECT * FROM users WHERE ".$premium_active." OR ".$vip_active." OR ".$private_active." ORDER by user_id DESC");
+$premium = "duration > 0 AND is_freeze = 0";
+$vip = "is_freeze = 0 AND vip_duration > 0";
+$private = "is_freeze = 0 AND private_duration > 0";
+
+$query = $mysqli->query("SELECT * FROM users
+WHERE ".$premium." OR ".$private." ORDER by user_id DESC");
 if($query->num_rows > 0)
 {
 	while($row = $query->fetch_assoc())
 	{
 		$data .= '';
 		$username = $row['user_name'];
-		$password = $row['user_pass'];
+		$password = decrypt_key($row['user_pass']);
+		$password = encryptor('decrypt',$password);		
 		$data .= 'yes | sh l2tp_add.sh '.$username.' '.$password.''.PHP_EOL;
 	}
 }
@@ -37,6 +114,7 @@ $fp = fopen($location, 'w');
 fwrite($fp, $data) or die("Unable to open file!");
 fclose($fp);
 
+
 #In-Active and Invalid Accounts
 $data2 = '';
 $premium_deactived = "duration <= 0";
@@ -44,9 +122,11 @@ $vip_deactived = "vip_duration <= 0";
 $private_deactived = "private_duration <= 0";
 $is_validated = "is_validated=0";
 $is_activate = "is_active=0";
-$freeze = "frozen=1";
+$freeze = "is_freeze=1";
+//$suspend = "suspend=1";
 
-$query2 = $mysqli->query("SELECT * FROM users WHERE ".$is_validated." OR ".$freeze." OR  ".$premium_deactived." OR ".$vip_deactived." OR ".$private_deactived."
+$query2 = $mysqli->query("SELECT * FROM users 
+WHERE ".$freeze." OR ".$premium_deactived ." AND ".$private_deactived." OR ".$is_activate."
 ");
 if($query2->num_rows > 0)
 {
@@ -64,4 +144,3 @@ fclose($fp);
 
 $mysqli->close();
 ?>
-1
